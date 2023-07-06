2fd2044c26eb3c1a5c7d91a355289859e5c584da
TranBach <traihonguyen2001@gmail.com>
2023-07-06 08:49:40 +0700

    install ngx-extended-pdf-viewer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
+import { KeoThaComponent } from './keo-tha/keo-tha.component';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, KeoThaComponent],
+  imports: [BrowserModule, AppRoutingModule, NgxExtendedPdfViewerModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
